a1261da12677da29bd9f13973b8b2c977d5af3db
Graham Hindle <grahamhindle@mac.com>
2011-06-21 22:23:16 +0100

    initial xcode project list
    Signed-off-by: Graham Hindle <grahamhindle@mac.com>

---
+++ Homepwner/Homepwner/HomepwnerAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  HomepwnerAppDelegate.h
+//  Homepwner
+//
+//  Created by Graham Hindle on 21/06/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomepwnerAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
